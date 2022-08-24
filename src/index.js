93dad0de9f9d0c8c70b23faff809eb4fa9de100d
pdrusr <pdrusr@gmail.com>
2022-08-24 10:08:33 -0300

    Consertando

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
 
-import Rotas from "./Rotas"
+import Inicio from "./Inicio"
 
 const contetor = document.getElementById("superelatorio")
 const origem = createRoot(contetor)
 
-origem.render(<Rotas/>)
+origem.render(<Inicio/>)
