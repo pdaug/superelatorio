8f72fbad5cd0fcefb60546f5085d1e586c1678f4
Pedro Augusto <dev.pdaug@gmail.com>
2023-04-22 16:31:52 -0300

    Update version to 3.0

--- script.js
+++ script.js
@@ -1,107 +1,60 @@
-const entradaFase = document.querySelector("select[name=fase]")
-const entradaAula = document.querySelector("select[name=aula]")
-const entradaSemana = document.querySelector("select[name=semana]")
-const entradaMaterial = document.querySelector("textarea[name=material]")
-const entradaResumo = document.querySelector("textarea[name=resumo]")
-const entradaTarefa = document.querySelector("textarea[name=tarefa]")
-const entradaInstrutorRelatorio = document.querySelector("input[name=instrutorrelatorio]")
-
-entradaInstrutorRelatorio.value = localStorage.getItem("instrutor")
-
+const { jsPDF } = window.jspdf
+document.querySelector("input[name=instrutorrelatorio]").value = localStorage.getItem("instrutor")
+document.querySelector("input[name=instrutorfeedback]").value = localStorage.getItem("instrutor")
 function CopiarRelatorio() {
-
-    const textoFase  = entradaFase.value
-    const textoAula  = entradaAula.value
-    const textoSemana  = entradaSemana.value
-    const textoMaterial  = entradaMaterial.value
-    const textoResumo  = entradaResumo.value
-    const textoTarefa  = entradaTarefa.value
-    const textoInstrutor  = entradaInstrutorRelatorio.value
-
-    const mensagem = `*${ textoFase } - ${ textoAula } - ${ textoSemana }*\n\n*Material de Referência*\n\n${ textoMaterial }\n\n*Resumo da Aula*\n\n${ textoResumo }\n\n*Diversão de Casa*\n\n${ textoTarefa }\n\n*Portal SuperGeeks*\n\n👉 https://portal.supergeeks.school/\n\nAtenciosamente\nInstrutor(a) ${ textoInstrutor }!`
-
-    navigator.clipboard.writeText(mensagem)
-
-        .then(function() {
-        
-            alert("Relatório copiado com sucesso!")
-
-            localStorage.setItem("instrutor", textoInstrutor)
-        
-        })
-    
-        .catch(function(erro) {
-        
-            alert("Falha ao copiar o Relatório!")
-        
-            console.log(erro)
-        
-        })
-
+    navigator.clipboard.writeText(`*${ document.querySelector("select[name=fase]").value } - ${ document.querySelector("select[name=aula]").value } - ${ document.querySelector("select[name=semana]").value }*\n\n*Material de Referência*\n\n${ document.querySelector("textarea[name=material]").value }\n\n*Resumo da Aula*\n\n${ document.querySelector("textarea[name=resumo]").value }\n\n*Diversão de Casa*\n\n${ document.querySelector("textarea[name=tarefa]").value }\n\n*Portal SuperGeeks*\n\n👉 https://portal.supergeeks.school/\n\nAtenciosamente\nInstrutor(a) ${ document.querySelector("input[name=instrutorrelatorio]").value }!`)
+    .then(function() {
+        alert("Relatório copiado com sucesso!")
+        localStorage.setItem("instrutor", document.querySelector("input[name=instrutorrelatorio]").value)
+    })
+    .catch(erro => console.log(erro))
 }
-
-const entradaFeedback = document.querySelector("select[name=feedback]")
-const entradaAluno = document.querySelector("input[name=aluno]")
-const entradaConteudo = document.querySelector("textarea[name=conteudo]")
-const entradaInstrutorFeedback = document.querySelector("input[name=instrutorfeedback]")
-
-entradaInstrutorFeedback.value = localStorage.getItem("instrutor")
-
 function CopiarFeedback() {
-
-    const textoFeedback = entradaFeedback.value
-    const textoAluno = entradaAluno.value
-    const textoConteudo = entradaConteudo.value
-    const textoInstrutor = entradaInstrutorFeedback.value
-
-    var aulasReferente
-
-    if (textoFeedback[0] == "1")
-        aulasReferente = "1 a 4"
-
-    else if (textoFeedback[0] == "2")
-        aulasReferente = "5 a 8"
-
-    else if (textoFeedback[0] == "3")
-        aulasReferente = "9 a 12"
-
-    else if (textoFeedback[0] == "4")
-        aulasReferente = "13 a 16"
-
-    const mensagem = `Olá! Aqui é o instrutor(a) ${ textoInstrutor } e esse é o ${ textoFeedback } do semestre (referente às aulas ${ aulasReferente }), nessa mensagem quero falar um pouco sobre o aluno e seu desempenho nas seguintes aulas.\n\n*Aluno(a): ${ textoAluno }*\n\n${ textoConteudo }\n\nAtenciosamente\nInstrutor(a) ${ textoInstrutor }!`
-
-    navigator.clipboard.writeText(mensagem)
-
-        .then(function() {
-    
-            alert("Feedback copiado com sucesso!")
-
-            localStorage.setItem("instrutor", textoInstrutor)
-        
-        })
-    
-        .catch(function(erro) {
-        
-            alert("Falha ao copiar o Feedback!")
-        
-            console.log(erro)
-        
-        })
-        
+    const textoFeedback = parseInt(document.querySelector("select[name=feedback]").value)
+    let aulasReferente = `${(textoFeedback * 4) - 3 } a ${textoFeedback * 4}`
+    navigator.clipboard.writeText(`Olá! Aqui é o instrutor(a) ${ document.querySelector("input[name=instrutorfeedback]").value } e esse é o ${ textoFeedback } do semestre (referente às aulas ${ aulasReferente }), nessa mensagem quero falar um pouco sobre o aluno e seu desempenho nas seguintes aulas.\n\n*Aluno(a): ${ document.querySelector("input[name=aluno]").value }*\n\n${ document.querySelector("textarea[name=conteudo]").value }\n\nAtenciosamente\nInstrutor(a) ${ document.querySelector("input[name=instrutorfeedback]").value }!`)
+    .then(() => {
+        alert("Feedback copiado com sucesso!")
+        localStorage.setItem("instrutor", document.querySelector("input[name=instrutorfeedback]").value)
+    })
+    .catch(erro => console.log(erro))
 }
-
-const menu = document.querySelector("div.menu")
-const janelaRelatorio = document.querySelector("div#relatorio")
-const janelaFeedback = document.querySelector("div#feedback")
-
 function AbrirFeedback() {
-    menu.setAttribute("style", "display: none;")
-    janelaRelatorio.setAttribute("style", "display: none;")
-    janelaFeedback.setAttribute("style", "display: flex;")
+    document.querySelector("div.menu").setAttribute("style", "display: none;")
+    document.querySelector("div#relatorio").setAttribute("style", "display: none;")
+    document.querySelector("div#feedback").setAttribute("style", "display: flex;")
 }
-
 function AbrirRelatorio() {
-    menu.setAttribute("style", "display: none;")
-    janelaRelatorio.setAttribute("style", "display: flex;")
-    janelaFeedback.setAttribute("style", "display: none;")
+    document.querySelector("div.menu").setAttribute("style", "display: none;")
+    document.querySelector("div#relatorio").setAttribute("style", "display: flex;")
+    document.querySelector("div#feedback").setAttribute("style", "display: none;")
+}
+function Voltar() {
+    document.querySelector("div.menu").setAttribute("style", "display: flex;")
+    document.querySelector("div#relatorio").setAttribute("style", "display: none;")
+    document.querySelector("div#feedback").setAttribute("style", "display: none;")
+}
+function SalvarFeedback() {
+    const doc = new jsPDF({ orientation: "l", unit: "px", format: [ 640, 640 ] })
+    doc.setFont("helvetica", "bold")
+    doc.setFontSize(32)
+    doc.text(document.querySelector("input[name=aluno]").value, 80, 80)
+    doc.setFont("helvetica", "normal")
+    doc.setFontSize(24)
+    doc.text("Instrutor(a) " + document.querySelector("input[name=instrutorfeedback]").value, 80, 128)
+    doc.setFontSize(22)
+    doc.text(doc.splitTextToSize(document.querySelector("textarea[name=conteudo]").value, 480), 80, 176)
+    doc.save(`${ document.querySelector("input[name=aluno]").value }.pdf`)
+}
+function SalvarRelatorio() {
+    const doc = new jsPDF({ orientation: "l", unit: "px", format: [ 640, 640 ] })
+    doc.setFont("helvetica", "bold")
+    doc.setFontSize(22)
+    doc.text(`${ document.querySelector("select[name=fase]").value } - ${ document.querySelector("select[name=aula]").value } - ${ document.querySelector("select[name=semana]").value }`, 80, 80)
+    doc.setFont("helvetica", "normal")
+    doc.text(`Instrutor(a) ${ document.querySelector("input[name=instrutorrelatorio]").value }`, 80, 128)
+    doc.text( doc.splitTextToSize( document.querySelector("textarea[name=material]").value, 480 ), 80, 176)
+    doc.text( doc.splitTextToSize( document.querySelector("textarea[name=resumo]").value, 480 ), 80, 272)
+    doc.text( doc.splitTextToSize( document.querySelector("textarea[name=tarefa]").value, 480 ), 80, 368)
+    doc.save(`${ document.querySelector("select[name=fase]").value } ${ document.querySelector("select[name=aula]").value }.pdf`)
 }
